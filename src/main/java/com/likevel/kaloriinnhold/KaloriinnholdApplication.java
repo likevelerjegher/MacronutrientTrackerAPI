e265a3ac3d509bb83646ae3c9a0d0074010ad9ba
likevelerjegher <134929928+likevelerjegher@users.noreply.github.com>
2024-02-18 16:25:35 +0300

    Add files via upload

---
+++ src/main/java/com/likevel/kaloriinnhold/KaloriinnholdApplication.java
@@ -0,0 +1,13 @@
+package com.likevel.kaloriinnhold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaloriinnholdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaloriinnholdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/likevel/kaloriinnhold/KaloriinnholdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likevel.kaloriinnhold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaloriinnholdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
