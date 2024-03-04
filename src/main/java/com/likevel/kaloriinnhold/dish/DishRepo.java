e1cbdf97499a18331774849bac7c73688ee08dc6
Hanna <134929928+likevelerjegher@users.noreply.github.com>
2024-03-04 14:45:09 +0300

    Delete src/main/java/com/likevel/kaloriinnhold/dish directory

--- src/main/java/com/likevel/kaloriinnhold/dish/DishRepo.java
+++
@@ -1,7 +0,0 @@
-package com.likevel.kaloriinnhold.dish;
-
-import org.springframework.http.ResponseEntity;
-
-public interface DishRepo {
-    ResponseEntity<Object> getNutritionalData(String ingredient, String weight);
-}
